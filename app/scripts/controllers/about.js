200f25b72ae84cc9360d041bb19c971f2358ef92
Ariel Swingley <ariel.swingley@berkeley.edu>
2017-02-07 20:35:40 -0800

    initial scaff

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the personalAngularApp
+ */
+angular.module('personalAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name personalAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the personalAngularApp
+ */
+angular.module('personalAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
